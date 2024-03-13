d2f48ac773352f98ef1e1712a90f1cc89c2e2e01
AVGVSTVS96 <bassim101@gmail.com>
2024-03-13 17:16:34 -0400

    fix #4 by updating `ModeToggle` script to rely on theme management in `index.astro`

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -10,22 +10,14 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<
-    "theme-light" | "dark" | "system"
-  >("theme-light");
-
-  React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    setThemeState(isDarkMode ? "dark" : "theme-light");
-  }, []);
-
-  React.useEffect(() => {
+  const setTheme = (selectedTheme: "theme-light" | "dark" | "system") => {
     const isDark =
-      theme === "dark" ||
-      (theme === "system" &&
+      selectedTheme === "dark" ||
+      (selectedTheme === "system" &&
         window.matchMedia("(prefers-color-scheme: dark)").matches);
     document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-  }, [theme]);
+    localStorage.setItem("theme", selectedTheme);
+  };
 
   return (
     <DropdownMenu>
@@ -37,13 +29,13 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
+        <DropdownMenuItem onClick={() => setTheme("theme-light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("dark")}>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("system")}>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
